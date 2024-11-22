ca2716c8c7c439c46dc85181ac97becddc88f4d3
Mooshaah <mmamra@uofcanada.edu.eg>
2024-11-22 21:59:02 +0200

    init project

--- src/main/java/com/example/librarymanagementsystem/Backend/DBConnector.java
+++ src/main/java/com/example/librarymanagementsystem/Backend/DBConnector.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 
 public class DBConnector {
     public void connect() throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/assignment";
+        String url = "jdbc:mysql://localhost:3306/assignment"; // placeholder URL, to be changed upon creating the real db
         String user = "root";
         String password = " ";
         DriverManager.getConnection(url,user,password);
